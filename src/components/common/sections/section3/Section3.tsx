f00ee0006b77b61d03a3a67de1686a56eb410042
eun <eun00864@gmail.com>
2025-03-17 19:04:57 +0900

    Add:[디자인] 섹션 3 추가

--- src/components/common/sections/section3/Section3.tsx
+++ src/components/common/sections/section3/Section3.tsx
@@ -1,22 +1,20 @@
 import style from './Section3.module.scss'
+import MoreButton from '../../buttons/morebutton/MoreButton'
 
 function Section3() {
   return (
     <section className={style.section_3_container}>
-    <div className={style.section_3_title_contianer}>
+    <div className={style.title_contianer}>
       <h2>Notices</h2>
     </div>
-    <div className={style.section_3_content_container}>
+    <div className={style.content_container}>
       <ul className={style.ul_container}>
         <li>로그인</li>
-        <li>나만의 코스</li>
-        <li>오늘의 코스</li>
-        <li>캘린더</li>
-        <li>공지</li>
-        <li>문의하기</li>
       </ul>
     </div>
-    <div className={style.section_3_buttom_container}></div>
+    <div className={style.section_3_buttom_container}>
+    <MoreButton/>
+    </div>
   </section>
   )
 }
