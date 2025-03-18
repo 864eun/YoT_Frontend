e08352a2d88ef1826a695519a099b30b4ac26a18
eun <eun00864@gmail.com>
2025-03-18 21:46:33 +0900

    Add:[디자인] 검색 바, 자세 카드 퍼블리싱

--- src/pages/my-course/MyCourse.tsx
+++ src/pages/my-course/MyCourse.tsx
@@ -1,11 +1,45 @@
-import React from 'react'
+import style from './MyCourse.module.scss';
+import SearchIcon from '@mui/icons-material/Search';
+import PoseCard from '../../components/common/cards/pose/PoseCard';
 
 function MyCourse() {
   return (
-    <div>
-      
+    <div className={style.mycourse_container}>
+      <section>
+        <div className={style.title_container}>
+          <h2>자세 선택</h2>
+        </div>
+        <div className={style.search_container}>
+          <div className={style.search_bar}>
+            <SearchIcon className={style.search_icon} />
+            <input
+              type="text"
+              placeholder="요가 자세를 검색해보세요"
+              className={style.input}
+            />
+          </div>
+        </div>
+        <div className={style.poseselect_container}>
+          <nav>
+            <ul>
+              <li>초급</li>
+              <li>중급</li>
+              <li>고급</li>
+            </ul>
+          </nav>
+        </div>
+
+          <div className={style.pose_cards_container}>
+            <div className={style.pose_cards_container_inner}>
+            <PoseCard />
+            <PoseCard />
+            <PoseCard />
+            <PoseCard />
+            </div>
+          </div>
+      </section>
     </div>
-  )
+  );
 }
 
-export default MyCourse
+export default MyCourse;
