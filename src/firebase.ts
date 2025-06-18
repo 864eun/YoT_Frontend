db1e4cc61546dbe02aba1ee0a098f43e17cee724
eun <eun00864@gmail.com>
2025-06-19 02:41:33 +0900

    Fix:firebase login 수정

--- src/firebase.ts
+++ src/firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyAbdw6207mtIwgzGF2xCmCBhmk-Rrcmc4Q',
-  authDomain: 'yot-frontend.firebaseapp.com',
-  projectId: 'yot-frontend',
-  storageBucket: 'yot-frontend.firebasestorage.app',
-  messagingSenderId: '906783138634',
-  appId: '1:906783138634:web:6067e589dd2a9eaae5d7b3',
-  measurementId: 'G-DPYK3TKQBY',
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
